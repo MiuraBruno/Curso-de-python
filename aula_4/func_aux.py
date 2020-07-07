eb58d60051afb9de650360f7acedc4ffa7b85739
brunomiura <brunolmiura@gmail.com>
2020-07-07 20:27:11 -0300

    aula 4

---
+++ aula_4/func_aux.py
@@ -0,0 +1,15 @@
+#contem funções auxiliares
+
+def hello():
+    return 'olá deu certo acessar outro arquivo'
+
+def cubo(n):
+    return n ** 3
+
+def check():
+    return __name__
+
+if __name__ == '__main__':
+    print('ola')
+    a = 1
+    print(a)
---
+++ aula_4/test.py
@@ -0,0 +1,1 @@
+import func_aux
